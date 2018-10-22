a8fccdc000be07b913907a34beb6831014095ec6
Alina.Kaltovich <Alina.Kaltovich@outlook.com>
2018-10-22 21:54:02 +0300

    first lesson

---
+++ main.js
@@ -0,0 +1,36 @@
+/*https://learn.javascript.ru/array*/
+
+/*var userPassword=prompt('Введите пороль');
+const originPassword='12345';
+
+if (userPassword===originPassword){
+    alert('ok');
+} else {
+    alert('wrong password');
+}
+
+let a=1 
+let b=2
+{
+alert(a+b);
+}
+*/
+
+//масив
+/*
+let arr=[1,2,3];
+for(let i=0; i<arr.length; i+=1){
+    console.info (arr[i]+1);
+}
+*/
+let userTesk=prompt ('Введите текст');
+
+
+
+function eat(arr) {
+    arr.pop();
+  }
+let arr=[prompt]
+
+ 
+console.log (arr.length);
